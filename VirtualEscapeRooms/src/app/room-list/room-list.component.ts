59b01d758ecd89d85c2085f70c566aff916fb122
Robyn Dunstan <DunstanR@members1st.org>
2022-09-08 13:53:00 -0400

    Build room list and first link

--- VirtualEscapeRooms/src/app/room-list/room-list.component.ts
+++ VirtualEscapeRooms/src/app/room-list/room-list.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.scss']
 })
 export class RoomListComponent implements OnInit {
+  rooms: Room[] = [
+    new Room(
+      "Snowman Adventure", 
+      "assets/snowman/snowman-icon.png", 
+      "snowman head",
+      "A Christmas-themed adventure to save a snowman from melting.", 
+      "/snowman")
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+class Room {
+  constructor(
+    public title: string, 
+    public imageUrl: string, 
+    public imageAlt: string, 
+    public description: string, 
+    public linkUrl: string) {}
 }
